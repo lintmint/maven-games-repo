e769a43f90f71e393dc2c546618e3db87a67650f
lintmint <lintmint@users.noreply.github.com>
2019-11-19 21:12:45 -0800

    Add files via upload

---
+++ src/main/java/com/john/allan/mavengames/MavenGamesApplication.java
@@ -0,0 +1,13 @@
+package com.john.allan.mavengames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john/allan/mavengames/MavenGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.allan.mavengames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
